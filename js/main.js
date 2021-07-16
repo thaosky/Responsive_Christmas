55403c391597ec0f637f8ed849ac2fdb4a77d378
thaosky <thaohsk@gmail.com>
2021-07-16 09:11:10 +0700

    Using flex box

--- js/main.js
+++ js/main.js
@@ -14,21 +14,22 @@ showMenu('nav-toggle', 'nav-menu')
 
 /*==================== ACTIVE ITEM ====================*/
 // Query all <section> that have 'id' attribute
-// const sections = document.querySelectorAll('section[id]');
-// const scrollActive = () => {
-//
-//     // method pageYOffset return a number...
-//     const scrollY = window.pageYOffset;
-//     sections.forEach(section => {
-//         const sectionId = section.getAttribute('id');
-//         const sectionHeight = section.offsetHeight; // return height of element
-//         const sectionTop = section.offsetTop; // return a number...
-//
-//         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-item');
-//         } else {
-//             document.querySelector(`.nav__menu a[href=${sectionId}]`).classList.remove('active-item');
-//         }
-//
-//     })
-// }
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+
+    // method pageYOffset return a number...
+    const scrollY = window.pageYOffset;
+    sections.forEach(section => {
+        const sectionId = section.getAttribute('id');
+        const sectionHeight = section.offsetHeight; // return height of element
+        const sectionTop = section.offsetTop; // return a number...
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-item')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-item')
+        }
+
+    })
+}
+window.addEventListener('scroll', scrollActive)
